cd6eedc7ce2631bb688dab59bf5667b8463e031e
Jean Amadeu <jean.amadeu2@gmail.com>
2021-03-08 08:50:37 -0300

    Renegociacao refatorada

--- src/main/java/br/com/zup/proposta/cartao/renegociacao/RenegociacaoResponse.java
+++ src/main/java/br/com/zup/proposta/cartao/renegociacao/RenegociacaoResponse.java
@@ -1,40 +1,38 @@
 package br.com.zup.proposta.cartao.renegociacao;
 
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-@Embeddable
-public class Renegociacao {
+public class RenegociacaoResponse {
 
     @NotBlank
-    @Column(nullable = false)
-    private String idRenegociacao;
+    private final String idRenegociacao;
 
     @NotNull
-    @Column(nullable = false)
-    private Integer quantidade;
+    private final Integer quantidade;
 
     @NotNull
-    @Column(nullable = false)
-    private BigDecimal valor;
+    private final BigDecimal valor;
 
     @NotNull
-    @Column(nullable = false)
-    private LocalDateTime dataDeCriacao;
+    private final LocalDateTime dataDeCriacao;
 
-    @Deprecated
-    public Renegociacao() {
-    }
-
-    public Renegociacao(@NotBlank String idRenegociacao, @NotNull Integer quantidade, @NotNull BigDecimal valor, @NotNull LocalDateTime dataDeCriacao) {
+    public RenegociacaoResponse(@NotBlank String idRenegociacao, @NotNull Integer quantidade, @NotNull BigDecimal valor, @NotNull LocalDateTime dataDeCriacao) {
         this.idRenegociacao = idRenegociacao;
         this.quantidade = quantidade;
         this.valor = valor;
         this.dataDeCriacao = dataDeCriacao;
+    }
+
+    public Renegociacao toModel() {
+        return new Renegociacao(
+                idRenegociacao,
+                quantidade,
+                valor,
+                dataDeCriacao
+        );
     }
 
     public String getIdRenegociacao() {
