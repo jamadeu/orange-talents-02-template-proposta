2c602f162f33e385b58651289d17c290da72108a
Jean Amadeu <jean.amadeu2@gmail.com>
2021-03-08 08:37:03 -0300

    Aviso refatorado

--- src/main/java/br/com/zup/proposta/cartao/aviso/AvisoResponse.java
+++ src/main/java/br/com/zup/proposta/cartao/aviso/AvisoResponse.java
@@ -1,25 +1,28 @@
 package br.com.zup.proposta.cartao.aviso;
 
 import javax.persistence.Column;
-import javax.persistence.Embeddable;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
-@Embeddable
-public class Aviso {
+
+public class AvisoResponse {
 
     @NotNull
     @Column(nullable = false)
-    private LocalDate validoAte;
+    private final LocalDate validoAte;
 
     @NotBlank
     @Column(nullable = false)
-    private String destino;
+    private final String destino;
 
-    public Aviso(@NotNull LocalDate validoAte, @NotBlank String destino) {
+    public AvisoResponse(@NotNull LocalDate validoAte, @NotBlank String destino) {
         this.validoAte = validoAte;
         this.destino = destino;
+    }
+
+    public Aviso toModel() {
+        return new Aviso(validoAte, destino);
     }
 
     public LocalDate getValidoAte() {
